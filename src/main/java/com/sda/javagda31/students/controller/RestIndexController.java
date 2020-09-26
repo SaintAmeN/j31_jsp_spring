06f7b8ee050a9b9a8c224512fe3c5459ba388e0d
amen <pawel.reclaw@gmail.com>
2020-09-26 11:15:33 +0200

    Update

--- src/main/java/com/sda/javagda31/students/controller/RestIndexController.java
+++ src/main/java/com/sda/javagda31/students/controller/RestIndexController.java
@@ -1,5 +1,27 @@
 package com.sda.javagda31.students.controller;
 
+import com.sda.javagda31.students.model.APIResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api")
 public class RestIndexController {
+
+    @GetMapping("/")
+    public APIResponse index() {
+        return new APIResponse("Helloł łorld!");
+    }
+
+    // Request param
+    // Path variable
+
+    // localhost:8080/api/welcome?imieTwe=...
+    // http://localhost:8080/api/welcome?imieTwe=pawel
+    @GetMapping("/welcome")
+    public APIResponse welcome(@RequestParam(name = "imieTwe") String imie) {
+        return new APIResponse("Welcome " + imie + "!");
+    }
 }
---
+++ src/main/java/com/sda/javagda31/students/model/APIResponse.java
@@ -0,0 +1,13 @@
+package com.sda.javagda31.students.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class APIResponse {
+    private String content;
+}
